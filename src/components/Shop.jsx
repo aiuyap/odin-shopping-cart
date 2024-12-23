eaf001b47d9be604b1a80a712d83e633012b72a0
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-24 00:34:29 +0800

    feat: managed to display a product from API

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,3 +1,45 @@
+import { useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Star } from "lucide-react";
+import { Button } from "./ui/button";
+
 export function Shop() {
-  return <div className="pt-12">Hello World</div>;
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setProducts(json);
+      });
+  }, []);
+
+  return (
+    <div className="p-14">
+      <Card className="max-w-lg">
+        <CardHeader>
+          <CardTitle>{products[0].title}</CardTitle>
+          <CardDescription>{products[0].description}</CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col items-center justify-center gap-2">
+          <img src={products[0].image} className="w-48" />
+          <p>
+            ${products[0].price}
+            <span className="flex items-end gap-1">
+              <Star className="fill-yellow-300" />
+              {products[0].rating.rate}
+            </span>
+          </p>
+          <Button>Add to Cart</Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
