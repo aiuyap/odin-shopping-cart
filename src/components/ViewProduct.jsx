c2f085df74bf208453ebec3b6239ebc189d92468
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-27 00:43:36 +0800

    feat: handled input for num of items -Added buttons to add or decrease items

--- src/components/ViewProduct.jsx
+++ src/components/ViewProduct.jsx
@@ -2,17 +2,35 @@ import { Star } from "lucide-react";
 import { useOutletContext, useParams } from "react-router-dom";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { useState } from "react";
 
 export function ViewProduct() {
   const products = useOutletContext();
   const params = useParams();
   const id = Number(params.id) - 1;
+  const [numOfItems, setNumOfItems] = useState(1);
+
+  function handleText(e) {
+    setNumOfItems(e.target.value);
+  }
+
+  function addNumOfItem() {
+    setNumOfItems((numOfItems) => numOfItems + 1);
+  }
+
+  function decreaseNumOfItem() {
+    if (numOfItems !== 1) {
+      setNumOfItems((numOfItems) => numOfItems - 1);
+    }
+  }
 
   return (
     <div className="flex items-center justify-center">
       <div className="grid max-w-2xl gap-2 p-14">
-        <h1 className="text-2xl font-bold">{products[id].title}</h1>
-        <h3 className="text-sm text-gray-500">{products[id].category}</h3>
+        <h1 className="text-center text-2xl font-bold">{products[id].title}</h1>
+        <h3 className="text-center text-sm text-gray-500">
+          {products[id].category}
+        </h3>
         <h2>{products[id].description}</h2>
         <img
           src={products[id].image}
@@ -24,9 +42,19 @@ export function ViewProduct() {
           {products[id].rating.count} reviews
         </p>
         <div className="flex justify-center gap-2">
-          <Button variant="outline">-</Button>
-          <Input type="number" className="w-20" />
-          <Button variant="outline">+</Button>
+          <Button variant="outline" onClick={decreaseNumOfItem}>
+            -
+          </Button>
+          <Input
+            value={numOfItems}
+            onChange={handleText}
+            type="number"
+            min="1"
+            className="w-20"
+          />
+          <Button variant="outline" onClick={addNumOfItem}>
+            +
+          </Button>
           <Button>Add to Cart</Button>
         </div>
       </div>
