29262786fbe494136eb75ec094f6245bdd5e9e63
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-12-31 03:25:39 +0800

    feat: completed checkout page features

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -26,67 +26,90 @@ export function Cart() {
     return (price * amount).toFixed(2);
   }
 
+  function calculateTotal() {
+    return itemOnCart.reduce((total, item) => {
+      return (
+        total +
+        Number(calculateItemSubtotal(products[item.id - 1].price, item.amount))
+      );
+    }, 0);
+  }
+
   return (
     <div className="flex justify-center p-14">
-      <Card className="p-4">
+      <Card className="min-w-80 p-4">
         <CardHeader>
           <CardTitle className="text-2xl">MY CART</CardTitle>
           <Separator />
         </CardHeader>
         <CardContent className="grid gap-4">
-          {itemOnCart.map((item) => {
-            return (
-              <div
-                key={item.id}
-                className="grid-cols-custom-grid grid items-center gap-4"
-              >
-                <img src={products[item.id - 1].image} className="w-14" />
-                <div className="grid">
-                  <h2>{products[item.id - 1].title}</h2>
-                  <div className="flex items-center gap-4">
-                    <p className="text-lg font-bold">
-                      $
-                      {calculateItemSubtotal(
-                        products[item.id - 1].price,
-                        item.amount,
-                      )}
-                    </p>
-                    <div className="flex items-center">
-                      <Button
-                        variant="ghost"
-                        onClick={() => changeItemAmount(item.id, false)}
-                      >
-                        -
-                      </Button>
-                      {item.amount}
-                      <Button
-                        variant="ghost"
-                        onClick={() => changeItemAmount(item.id, true)}
-                      >
-                        +
-                      </Button>
+          {itemOnCart.length === 0 ? (
+            <p className="text-gray-400"> Nothing in your cart yet :( </p>
+          ) : (
+            itemOnCart.map((item) => {
+              return (
+                <div
+                  key={item.id}
+                  className="grid-cols-custom-grid grid items-center gap-4"
+                >
+                  <img src={products[item.id - 1].image} className="w-14" />
+                  <div className="grid">
+                    <h2>{products[item.id - 1].title}</h2>
+                    <div className="flex items-center gap-4">
+                      <p className="text-lg font-bold">
+                        $
+                        {calculateItemSubtotal(
+                          products[item.id - 1].price,
+                          item.amount,
+                        )}
+                      </p>
+                      <div className="flex items-center">
+                        <Button
+                          variant="ghost"
+                          onClick={() => changeItemAmount(item.id, false)}
+                        >
+                          -
+                        </Button>
+                        {item.amount}
+                        <Button
+                          variant="ghost"
+                          onClick={() => changeItemAmount(item.id, true)}
+                        >
+                          +
+                        </Button>
+                      </div>
                     </div>
                   </div>
+                  <div>
+                    <Button
+                      variant="destructive"
+                      onClick={() => removeItem(item.id)}
+                    >
+                      <Trash2 />
+                    </Button>
+                  </div>
                 </div>
-                <div>
-                  <Button
-                    variant="destructive"
-                    onClick={() => removeItem(item.id)}
-                  >
-                    <Trash2 />
-                  </Button>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })
+          )}
         </CardContent>
         <CardHeader>
           <CardTitle className="text-2xl">TOTAL</CardTitle>
           <Separator />
         </CardHeader>
         <CardContent className="flex justify-between">
-          <p className="text-3xl font-bold">$69.69</p>
-          <Button>Checkout</Button>
+          <p className="text-3xl font-bold">
+            ${itemOnCart.length === 0 ? "0.00" : calculateTotal().toFixed(2)}
+          </p>
+          <Button
+            onClick={() =>
+              alert(
+                `Total amount to be paid is $${calculateTotal().toFixed(2)}. Checkout complete, wohoo!`,
+              )
+            }
+          >
+            Checkout
+          </Button>
         </CardContent>
       </Card>
     </div>
